1bde5c913e730da33b6b1b5635607cdf1384d9b2
EnzoNumber7 <117282665+EnzoNumber7@users.noreply.github.com>
2025-03-17 20:28:04 +0100

    [ADD] WindmillSkill Class

---
+++ Assets/_Project/_Scripts/Skills/WindmillSkill/WindmillSkill.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindmillSkill : Skill
+{
+    public WindmillSkill(Player player) : base(player)
+    {
+
+    }
+
+    public override void UseSkill()
+    {
+
+    }
+}
