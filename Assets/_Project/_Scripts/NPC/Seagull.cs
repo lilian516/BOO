e1fe5cccf77b338213e0b5293d99c9f9a03a6e79
lilian516 <115982783+lilian516@users.noreply.github.com>
2025-04-08 12:53:41 +0200

    [ADD] seagull vomit

--- Assets/_Project/_Scripts/NPC/Seagull.cs
+++ Assets/_Project/_Scripts/NPC/Seagull.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Seagull : MonoBehaviour
 {
+
+    [SerializeField] Animator _animator;
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Player>() != null)
         {
             // Ajouter l'animation de chier sur Boo
-
+            _animator.SetTrigger("Vomit");
             AngrySystem.Instance.ChangeAngryLimits();
         }
     }
