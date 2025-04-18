cdd0c3294ef05e133282a8f56dfdbc2010d25043
lilian516 <115982783+lilian516@users.noreply.github.com>
2025-04-18 13:26:13 +0200

    [FIX] build

--- Assets/_Project/_Scripts/Player/Player.cs
+++ Assets/_Project/_Scripts/Player/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 public class Player : MonoBehaviour, IChangeable
 {
