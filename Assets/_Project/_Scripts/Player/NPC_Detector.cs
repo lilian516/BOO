ea179cefc7c916919e71df266f6ad8f5bbf09ecc
Victoraş <vmartinan@gaming.tech>
2025-04-18 10:09:45 +0200

    [АШЧ] Ырууз щгедшту ырйвук

--- Assets/_Project/_Scripts/Player/NPC_Detector.cs
+++ Assets/_Project/_Scripts/Player/NPC_Detector.cs
@@ -13,22 +13,24 @@ public class NPC_Detector : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<ISpeakable>() != null)
+        if (other.gameObject.GetComponent<ISpeakable>() != null || other.gameObject.GetComponent<IClickable>() != null)
         {
             OnDetectNPC?.Invoke();
             _npcInRange++;
 
-            other.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().material.SetFloat("_OutlineSize", 3.0f);
+            if (other.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>() != null)
+                other.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().material.SetFloat("_OutlineSize", 3.0f);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.GetComponent<ISpeakable>() != null)
+        if (other.gameObject.GetComponent<ISpeakable>() != null || other.gameObject.GetComponent<IClickable>() != null)
         {
             _npcInRange--;
 
-            other.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().material.SetFloat("_OutlineSize", 0.0f);
+            if (other.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>() != null)
+                other.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().material.SetFloat("_OutlineSize", 0.0f);
 
             if (_npcInRange == 0)
                 OnStopDetectNPC?.Invoke();
