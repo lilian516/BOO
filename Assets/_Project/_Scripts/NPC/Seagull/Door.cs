c948c18f4a4d0c36240ce5fad7b3efbf919124cd
lilian516 <115982783+lilian516@users.noreply.github.com>
2025-04-24 12:09:06 +0200

    [MERGE] build

--- Assets/_Project/_Scripts/NPC/Seagull/Door.cs
+++ Assets/_Project/_Scripts/NPC/Seagull/Door.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.ProBuilder.Shapes;
-using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 public class Door : MonoBehaviour, IClickable
 {
