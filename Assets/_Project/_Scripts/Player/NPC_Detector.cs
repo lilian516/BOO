a9738516e6552f10e44f984d12860531aad9697f
Victoraş <vmartinan@gaming.tech>
2025-04-24 10:10:07 +0200

    [FIX] No outline when Boo mad

--- Assets/_Project/_Scripts/Player/NPC_Detector.cs
+++ Assets/_Project/_Scripts/Player/NPC_Detector.cs
@@ -2,27 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC_Detector : MonoBehaviour
+public class NPC_Detector : MonoBehaviour, IChangeable
 {
     public delegate void DetectNPC();
     public DetectNPC OnDetectNPC;
     public delegate void StopDetectNPC();
     public StopDetectNPC OnStopDetectNPC;
 
+    private List<Collider> _encounteredColliders;
+
     private int _npcInRange = 0;
+
+    private void Start()
+    {
+        _encounteredColliders = new List<Collider>();
+
+        AngrySystem.Instance.OnChangeElements += Change;
+        AngrySystem.Instance.OnResetElements += ResetChange;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<ISpeakable>() != null || other.gameObject.GetComponent<IClickable>() != null)
+        if (ToggleOutlineWithAnger(other))
         {
+            _encounteredColliders.Add(other);
             OnDetectNPC?.Invoke();
             _npcInRange++;
-
-            if (other.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>() != null)
-                other.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().material.SetFloat("_OutlineSize", 3.0f);
-
-            if (other.GetComponent<Door>() != null)
-                other.GetComponent<Door>().DoorOutlineMaterial.SetFloat("_Outline_Thickness", 0.01f);
         }
     }
 
@@ -40,6 +45,61 @@ public class NPC_Detector : MonoBehaviour
 
             if (_npcInRange == 0)
                 OnStopDetectNPC?.Invoke();
+
+            _encounteredColliders.Remove(other);
+        }
+    }
+
+    private bool ToggleOutlineWithAnger(Collider other)
+    {
+        if (other.gameObject.GetComponent<ISpeakable>() != null || other.gameObject.GetComponent<IClickable>() != null)
+        {
+            if (AngrySystem.Instance != null)
+            {
+                if (AngrySystem.Instance.IsAngry)
+                {
+                    if (other.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>() != null)
+                        other.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().material.SetFloat("_OutlineSize", 0.0f);
+
+                    if (other.GetComponent<Door>() != null)
+                        other.GetComponent<Door>().DoorOutlineMaterial.SetFloat("_Outline_Thickness", 0.0f);
+                }
+                else
+                {
+                    if (other.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>() != null)
+                        other.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().material.SetFloat("_OutlineSize", 3.0f);
+
+                    if (other.GetComponent<Door>() != null)
+                        other.GetComponent<Door>().DoorOutlineMaterial.SetFloat("_Outline_Thickness", 0.01f);
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    public void Change()
+    {
+        if (_encounteredColliders.Count <= 0)
+            return;
+
+        foreach (Collider other in _encounteredColliders)
+        {
+            Debug.Log("boobooboo");
+            ToggleOutlineWithAnger(other);
+        }
+    }
+
+    public void ResetChange()
+    {
+        if (_encounteredColliders.Count <= 0)
+            return;
+
+        foreach (Collider other in _encounteredColliders)
+        {
+            if (other != null)
+                ToggleOutlineWithAnger(other);
         }
     }
 
@@ -47,4 +107,5 @@ public class NPC_Detector : MonoBehaviour
     {
         GetComponent<SphereCollider>().radius = radius;
     }
+
 }
