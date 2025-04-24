7792e56bcb0e4218b5be1f547a1354169c189e24
EnzoNumber7 <117282665+EnzoNumber7@users.noreply.github.com>
2025-04-24 16:44:47 +0200

    [FIX] Angry Stick blocked for transition

--- Assets/_Project/_Scripts/UI/JoystickUI.cs
+++ Assets/_Project/_Scripts/UI/JoystickUI.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.OnScreen;
 
-public class JoystickUI : MonoBehaviour
+public class JoystickUI : MonoBehaviour, IChangeable
 {
 
     private Vector3 _centerPos;
     private GameObject _middleCircle;
     private GameObject _joystick;
+    private float _baseRange;
+
     void Start()
     {
         _centerPos = transform.GetChild(0).position;
         _middleCircle = transform.GetChild(1).gameObject;
         _joystick = transform.GetChild(2).gameObject;
+        _baseRange = _joystick.GetComponent<OnScreenStick>().movementRange;
+
+        AngrySystem.Instance.OnChangeElements += Change;
+        AngrySystem.Instance.OnResetElements += ResetChange;
     }
 
     void Update()
@@ -23,4 +29,25 @@ public class JoystickUI : MonoBehaviour
             _middleCircle.transform.position = _centerPos + (_joystick.transform.position - _centerPos) / 3;
         }
     }
+    public void Change()
+    {
+        StartCoroutine(BlockControl());
+    }
+
+    public void ResetChange()
+    {
+        StartCoroutine(BlockControl());
+    }
+
+    private IEnumerator BlockControl()
+    {
+        _joystick.GetComponent<OnScreenStick>().movementRange = 0;
+        _joystick.transform.position = _centerPos;
+        _middleCircle.transform.position = _centerPos;
+
+        yield return new WaitForSeconds(3.7f);
+
+        _joystick.GetComponent<OnScreenStick>().movementRange = _baseRange;
+    }
 }
+
