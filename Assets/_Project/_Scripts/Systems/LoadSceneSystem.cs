9d68edc46775785e60e1345c8f2bd26757b7690f
UVictoras <vmartinan@gaming.tech>
2025-04-26 16:17:53 +0200

    [ADD] Loading Screen

--- Assets/_Project/_Scripts/Systems/LoadSceneSystem.cs
+++ Assets/_Project/_Scripts/Systems/LoadSceneSystem.cs
@@ -4,8 +4,15 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneSystem : Singleton<LoadSceneSystem>
 {
+    [SerializeField] private GameObject _loadingObject;
+    [SerializeField] private Animator _loadingScreenAnimator;
     public IEnumerator LoadTargetScenes(string[] targetScenes)
     {
+        _loadingObject.SetActive(true);
+        _loadingScreenAnimator.SetBool("IsLoading", true);
+
+        yield return new WaitForSeconds(1.0f);
+
         foreach (string scene in targetScenes)
         {
             AsyncOperation sceneOperation = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
@@ -15,6 +22,8 @@ public class LoadSceneSystem : Singleton<LoadSceneSystem>
             }
             yield return new WaitUntil(() => sceneOperation.isDone);
         }
+        _loadingObject.SetActive(false);
+        _loadingScreenAnimator.SetBool("IsLoading", false);
     }
     public IEnumerator UnloadTargetScenes(string[] targetScenes)
     {
