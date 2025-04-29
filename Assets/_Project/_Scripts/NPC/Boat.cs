23310f8d84582905491de5ca63001e2ec7178a1c
lilian516 <115982783+lilian516@users.noreply.github.com>
2025-04-29 14:40:05 +0200

    [FIX] anim arbre

--- Assets/_Project/_Scripts/NPC/Boat.cs
+++ Assets/_Project/_Scripts/NPC/Boat.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Boat : MonoBehaviour, IInteractable
 {
     private bool _hasInteractedWithStick = false;
+    [SerializeField] Animator _animator;
     public void Interact(PlayerSkill playerSkill)
     {
         switch (playerSkill)
         {
             case PlayerSkill.StickSkill:
                 _hasInteractedWithStick = true;
+                _animator.SetTrigger("Baton");
                 break;
             case PlayerSkill.PantsSkill:
                 if (_hasInteractedWithStick)
                 {
+                    _animator.SetTrigger("Slip");
                     Debug.Log("A placé le slip sur le mât");
                 }
                 else
