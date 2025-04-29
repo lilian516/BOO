530a6f4d945eab19a816efe96d8a22f5fc64019b
EnzoNumber7 <117282665+EnzoNumber7@users.noreply.github.com>
2025-04-29 15:10:31 +0200

    [FIX] Boo Child Dialogue

--- Assets/_Project/_Scripts/Player/StateMachine/ConcreteStates/PlayerSpeakingState.cs
+++ Assets/_Project/_Scripts/Player/StateMachine/ConcreteStates/PlayerSpeakingState.cs
@@ -20,10 +20,14 @@ public class PlayerSpeakingState : PlayerState
         DialogueSystem.Instance.OnChoice += OnChoiceSelection;
         DialogueSystem.Instance.OnEndDialogue += OnExitSpeakState;
 
+        GameManager.Instance.Player.GetComponent<Player>().PlayerFaceAnimator.enabled = false;
+        GameManager.Instance.Player.GetComponent<Player>().PlayerFaceAnimator.gameObject.GetComponent<SpriteRenderer>().sprite = null;
+
     }
 
     public override void ExitState()
     {
+        GameManager.Instance.Player.GetComponent<Player>().PlayerFaceAnimator.enabled = true;
         _player.Input.OnSpeak -= DialogueSystem.Instance.AdvanceDialogue;
         DialogueSystem.Instance.OnChoice -= OnChoiceSelection;
         DialogueSystem.Instance.OnEndDialogue -= OnExitSpeakState;
